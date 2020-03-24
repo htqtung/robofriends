28393e808fd24b1dacb6e18610fbf57151ddbeda
Tung Huynh <htqtung@gmail.com>
2020-03-25 01:12:04 +0200

    add title

---
+++ src/CardList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {robots.map((item, index) => {
+        return (
+          <Card key={index} id={item.id} name={item.name} email={item.email} />
+        );
+      })}
+    </div>
+  );
+};
+
+export default CardList;
