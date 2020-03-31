ae0448c78e52b59636b90dc12d95870c7f49f7e1
Tung Huynh <htqtung@gmail.com>
2020-04-01 00:27:57 +0300

    add react hooks, search box

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from './CardList';
-import robots from './robots';
+import SearchBox from './SearchBox';
+import './App.css';
 
 const App = () => {
-  return (
-    <div>
-      <h1>RoboFriends</h1>
-      <CardList robots={robots} />
-    </div>
-  );
+  const [searchWord, setSearchWord] = useState('');
+  const [robotArray, setRobotArray] = useState([]);
+  /*
+  fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+    response.json();
+  }).then(users => {
+    this.setState({robots: users})
+  })
+  */
+  useEffect(() => {
+    const filteredRobots = robotArray.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchWord.toLowerCase());
+    });
+    setRobotArray(filteredRobots);
+  }, [robotArray, searchWord]);
+  if (robotArray.length === 0) {
+    return <h1>Loading</h1>;
+  } else {
+    return (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchWord={searchWord} setSearchWord={setSearchWord} />
+        <CardList robots={robotArray} />
+      </div>
+    );
+  }
 };
 
 export default App;
---
+++ src/SearchBox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SearchBox = ({ searchWord, setSearchWord }) => {
+  return (
+    <div className="pa2">
+      <input
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search robots"
+        value={searchWord}
+        onChange={(e) => setSearchWord(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
